6984a5ca0df8a9ed612379361ac4c17eeeb30ebb
Razine Bensari <bensaria97@gmail.com>
2019-06-17 22:55:10 -0400

    forgotten changes

--- cmd/edit.go
+++ cmd/edit.go
@@ -11,7 +11,7 @@ func init() {
 var edit = &cobra.Command{
 	Use: "edit",
 	short: "Edits a google task",
-	Long: "Edits a google task given the correct parameter and updates it in your google task",
+	Long: "Edits a google task given the correct parameter and updates it in your google task.",
 	Run: func(cmd *cobra.Command, args []String){
 		//do something
 	}

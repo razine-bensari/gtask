c0a9d02d63a87d91ac6bcb9836ccb7f5c0ecdb73
Razine Bensari <razine.bensari@sap.com>
2019-07-10 12:39:28 -0400

    adding client method with query

--- client/client.go
+++ client/client.go
@@ -1,16 +1,16 @@
 package client
 
-
 import (
-	"github.com/parnurzeal/gorequest"
 	gtask "gtask/domain"
+
+	"gorequest"
 )
 
 //GoogleTaskClient ...
 type GoogleTaskClient struct {
 	request *gorequest.SuperAgent
 	baseURL string
-	token string
+	token   string
 }
 
 //NewGoogleTaskClient ...
@@ -24,109 +24,152 @@ func NewGoogleTaskClient(token string) *GoogleTaskClient {
 
 //Returns all the authenticated user's task lists ...
 func (googleTaskClient GoogleTaskClient) getAllLists() (gorequest.Response, string, []error) {
-	endpoint :=  googleTaskClient.baseURL + "/users/@me/lists" //build endpoint url
+	endpoint := googleTaskClient.baseURL + "/users/@me/lists" //build endpoint url
 	//Build Request
 	return googleTaskClient.request.
-			Get(endpoint).
-			Set("access-token", "my-access-token"). //TODO: Add function that goes and fetches the access token
-			End() //Needed after each request
+		Get(endpoint).
+		Set("access-token", "my-access-token"). //TODO: Add function that goes and fetches the access token
+		End()                                   //Needed after each request
 }
+
+//Returns all the authenticated user's task lists ...
+func (googleTaskClient GoogleTaskClient) getAllListsWithQuery(query string) (gorequest.Response, string, []error) {
+	endpoint := googleTaskClient.baseURL + "/users/@me/lists" //build endpoint url
+	//Build Request
+	return googleTaskClient.request.
+		Get(endpoint).
+		Set("access-token", "my-access-token"). //TODO: Add function that goes and fetches the access token
+		Query(query).
+		End() //Needed after each request
+}
+
 //Returns the authenticated user's specified task list ...
 func (googleTaskClient GoogleTaskClient) getTaskList(taskList gtask.TaskList) (gorequest.Response, string, []error) {
-	endpoint :=  googleTaskClient.baseURL + "/users/@me/lists/" + taskList.ID() //build endpoint url
+	endpoint := googleTaskClient.baseURL + "/users/@me/lists/" + taskList.ID() //build endpoint url
 	//Build Request
 	return googleTaskClient.request.
-			Get(endpoint).
-			Set("access-token", "my-access-token").//TODO
-			End()
+		Get(endpoint).
+		Set("access-token", "my-access-token"). //TODO
+		End()
 }
+
 //Creates a new task list and adds it to the authenticated user's task lists ...
 func (googleTaskClient GoogleTaskClient) createTaskList(taskList gtask.TaskList) (gorequest.Response, string, []error) {
-	endpoint :=  googleTaskClient.baseURL + "/users/@me/lists" 
+	endpoint := googleTaskClient.baseURL + "/users/@me/lists"
 	return googleTaskClient.request.
-			Post(endpoint).
-			Set("access-token", "my-access-token").//TODO
-			Set("Accept","application/json").
-			Send(taskList). //GoRequest marshals struct into json format using Send()
-			End()
+		Post(endpoint).
+		Set("access-token", "my-access-token"). //TODO
+		Set("Accept", "application/json").
+		Send(taskList). //GoRequest marshals struct into json format using Send()
+		End()
 }
+
 //Updates the authenticated user's specified task list ...
 func (googleTaskClient GoogleTaskClient) updateTaskList(taskList gtask.TaskList) (gorequest.Response, string, []error) {
-	endpoint :=  googleTaskClient.baseURL + "/users/@me/lists/" + taskList.ID()
+	endpoint := googleTaskClient.baseURL + "/users/@me/lists/" + taskList.ID()
 	return googleTaskClient.request.
-			Put(endpoint).
-			Set("access-token", "my-access-token").//TODO
-			Set("Accept","application/json").
-			Send(taskList).
-			End()
+		Put(endpoint).
+		Set("access-token", "my-access-token"). //TODO
+		Set("Accept", "application/json").
+		Send(taskList).
+		End()
 }
+
 //Deletes the authenticated user's specified task list ...
 func (googleTaskClient GoogleTaskClient) deleteTaskList(taskList gtask.TaskList) (gorequest.Response, string, []error) {
-	endpoint :=  googleTaskClient.baseURL + "/users/@me/lists/" + taskList.ID()
+	endpoint := googleTaskClient.baseURL + "/users/@me/lists/" + taskList.ID()
 	return googleTaskClient.request.
-			Delete(endpoint).
-			Set("access-token", "my-access-token").//TODO
-			End()
+		Delete(endpoint).
+		Set("access-token", "my-access-token"). //TODO
+		End()
 }
+
 //Returns all tasks in the specified task list.
 func (googleTaskClient GoogleTaskClient) getAllTasksInList(taskList gtask.TaskList) (gorequest.Response, string, []error) {
-	endpoint :=  googleTaskClient.baseURL + "/lists/" + taskList.ID() + "tasks"
+	endpoint := googleTaskClient.baseURL + "/lists/" + taskList.ID() + "tasks"
 	return googleTaskClient.request.
-			Get(endpoint).
-			Set("access-token", "my-access-token").//TODO
-			End()
+		Get(endpoint).
+		Set("access-token", "my-access-token"). //TODO
+		End()
 }
+
+//Returns all tasks in the specified task list.
+func (googleTaskClient GoogleTaskClient) getAllTasksInListWithQuery(taskList gtask.TaskList, query string) (gorequest.Response, string, []error) {
+	endpoint := googleTaskClient.baseURL + "/lists/" + taskList.ID() + "tasks"
+	return googleTaskClient.request.
+		Get(endpoint).
+		Set("access-token", "my-access-token"). //TODO
+		Query(query).
+		End()
+}
+
 //Returns the specified task.
 func (googleTaskClient GoogleTaskClient) getTaskInList(taskList gtask.TaskList, task gtask.Task) (gorequest.Response, string, []error) {
-	endpoint :=  googleTaskClient.baseURL + "/lists/" + taskList.ID() + "/tasks/" + task.ID()
+	endpoint := googleTaskClient.baseURL + "/lists/" + taskList.ID() + "/tasks/" + task.ID()
 	return googleTaskClient.request.
-			Get(endpoint).
-			Set("access-token", "my-access-token").//TODO
-			End()
+		Get(endpoint).
+		Set("access-token", "my-access-token"). //TODO
+		End()
 }
+
 //Creates a new task on the specified task list.
 func (googleTaskClient GoogleTaskClient) createTaskInList(taskList gtask.TaskList, task gtask.Task) (gorequest.Response, string, []error) {
-	endpoint :=  googleTaskClient.baseURL + "/lists/" + taskList.ID() + "tasks"
+	endpoint := googleTaskClient.baseURL + "/lists/" + taskList.ID() + "tasks"
 	return googleTaskClient.request.
-			Post(endpoint).
-			Set("access-token", "my-access-token").//TODO
-			Set("Accept","application/json").
-			Send(task).
-			End()
+		Post(endpoint).
+		Set("access-token", "my-access-token"). //TODO
+		Set("Accept", "application/json").
+		Send(task).
+		End()
 }
+
 //Updates the specified task.
 func (googleTaskClient GoogleTaskClient) updateTaskInList(taskList gtask.TaskList, task gtask.Task) (gorequest.Response, string, []error) {
-	endpoint :=  googleTaskClient.baseURL + "/lists/" + taskList.ID() + "/tasks/" + task.ID()
+	endpoint := googleTaskClient.baseURL + "/lists/" + taskList.ID() + "/tasks/" + task.ID()
 	return googleTaskClient.request.
-			Put(endpoint).
-			Set("access-token", "my-access-token").//TODO
-			Set("Accept","application/json").
-			Send(task).
-			End()
+		Put(endpoint).
+		Set("access-token", "my-access-token"). //TODO
+		Set("Accept", "application/json").
+		Send(task).
+		End()
 }
+
 //Deletes the specified task from the task list.
 func (googleTaskClient GoogleTaskClient) deleteTaskInList(taskList gtask.TaskList, task gtask.Task) (gorequest.Response, string, []error) {
-	endpoint :=  googleTaskClient.baseURL + "/lists/" + taskList.ID() + "/tasks/" + task.ID()
+	endpoint := googleTaskClient.baseURL + "/lists/" + taskList.ID() + "/tasks/" + task.ID()
 	return googleTaskClient.request.
-			Delete(endpoint).
-			Set("access-token", "my-access-token").//TODO
-			End()
+		Delete(endpoint).
+		Set("access-token", "my-access-token"). //TODO
+		End()
 }
+
 //Clears all completed tasks from the specified task list.
 //The affected tasks will be marked as 'hidden' and no longer be returned by default when retrieving all tasks for a task list.
 func (googleTaskClient GoogleTaskClient) clearAllTasksInTaskList(taskList gtask.TaskList) (gorequest.Response, string, []error) {
-	endpoint :=  googleTaskClient.baseURL + "/lists/" + taskList.ID() + "/clear"
+	endpoint := googleTaskClient.baseURL + "/lists/" + taskList.ID() + "/clear"
 	return googleTaskClient.request.
-			Post(endpoint).
-			Set("access-token", "my-access-token").//TODO
-			End()
+		Post(endpoint).
+		Set("access-token", "my-access-token"). //TODO
+		End()
 }
+
 //Moves the specified task to another position in the task list.
 //This can include putting it as a child task under a new parent and/or move it to a different position among its sibling tasks.
 func (googleTaskClient GoogleTaskClient) moveTaskInTaskList(taskList gtask.TaskList, task gtask.Task) (gorequest.Response, string, []error) {
-	endpoint :=  googleTaskClient.baseURL + "/lists/" + taskList.ID() + "/tasks/" + task.ID() + "move"
+	endpoint := googleTaskClient.baseURL + "/lists/" + taskList.ID() + "/tasks/" + task.ID() + "move"
 	return googleTaskClient.request.
-			Post(endpoint).
-			Set("access-token", "my-access-token").//TODO
-			End()
+		Post(endpoint).
+		Set("access-token", "my-access-token"). //TODO
+		End()
+}
+
+//Moves the specified task to another position in the task list.
+//This can include putting it as a child task under a new parent and/or move it to a different position among its sibling tasks.
+func (googleTaskClient GoogleTaskClient) moveTaskInTaskListWithQuery(taskList gtask.TaskList, task gtask.Task, query string) (gorequest.Response, string, []error) {
+	endpoint := googleTaskClient.baseURL + "/lists/" + taskList.ID() + "/tasks/" + task.ID() + "move"
+	return googleTaskClient.request.
+		Post(endpoint).
+		Set("access-token", "my-access-token"). //TODO
+		Query(query).
+		End()
 }
